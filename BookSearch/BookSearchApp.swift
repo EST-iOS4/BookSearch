1e6658452c2d6009b7e87dd2b63afebc3a5094a4
배정만 <jungman82@gmail.com>
2025-09-26 10:46:56 +0900

    Initial Commit

---
+++ BookSearch/BookSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchApp.swift
+//  BookSearch
+//
+//  Created by Jungman Bae on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
